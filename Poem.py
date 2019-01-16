f5b58bb23af52f1fa29e73eb143782655c6fe157
Tuguldur <tuguldurbold5@gmail.com>
2019-01-15 16:13:58 -0800

    Initial commit, first day of lab

---
+++ Poem.py
@@ -0,0 +1,1 @@
+print("Roses are red \nViolets are blue \nSugar is sweet \nBut I have committment issues \nSo I'd rather just be friends \nAt this point in our relationship.")
---
+++ Temperature.py
@@ -0,0 +1,4 @@
+temperature_in_celcius = 10
+temperature_in_fahrenheit = (temperature_in_celcius * 9/5) + 32
+print(temperature_in_celcius)
+print(temperature_in_fahrenheit)
